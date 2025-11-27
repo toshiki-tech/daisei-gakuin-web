3fb4d68090435a73f53f211900f91dddc2830caf
skyfuture\dubiaoqi <dubiaoqi@sky-future.com>
2025-11-27 15:31:16 +0900

    Add favicon and app icons configuration

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import './globals.css'
 
+const basePath = process.env.NEXT_PUBLIC_BASE_PATH || ''
+
 export default function RootLayout({
   children,
 }: {
@@ -11,6 +13,13 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300;400;500;600;700&family=Noto+Sans+SC:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
+        
+        {/* Favicon and Icons */}
+        <link rel="icon" type="image/x-icon" href={`${basePath}/favicon.ico`} />
+        <link rel="icon" type="image/png" sizes="32x32" href={`${basePath}/daisei-icons/favicon-32.png`} />
+        <link rel="icon" type="image/png" sizes="16x16" href={`${basePath}/daisei-icons/favicon-16.png`} />
+        <link rel="apple-touch-icon" sizes="180x180" href={`${basePath}/daisei-icons/apple-touch-icon-180.png`} />
+        <link rel="manifest" href={`${basePath}/manifest.json`} />
       </head>
       <body>{children}</body>
     </html>
